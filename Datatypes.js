60e449ee77c24302125de05d097d63263bcd30b4
KartikXx <kartik.rajput1714@gmail.com>
2023-08-18 23:57:13 +0530

    Dataypes

---
+++ Datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; //treat all JS code as newer version
+
+
+// alert(4+5) cannot run this in nodejs rn.
+ //Code readability is priority:)
+ let name ="Kartik" //String
+ let age = 18 // int
+ let isLoggedIn= true //boolean
+ /*
+number
+bigint
+string=> ""
+boolean => true|false
+null=> Standalone value | Empty value | null is an object
+undefined=> when value is not assigned only variable is decalared
+symbol=> unique
+
+Object
+*/
+
+console.log(typeof "Kartik")
+console.log(typeof age)
+console.log(typeof isLoggedIn)
+console.log(typeof undefined)
+console.log(typeof null)
