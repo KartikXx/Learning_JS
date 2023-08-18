a2d2df51a0ab93e84c923557d9d8881f42a866c3
KartikXx <kartik.rajput1714@gmail.com>
2023-08-19 00:24:51 +0530

    On branch main  Your branch is up to date with 'origin/main'.  Changes to be committed: 	modified:   Basics/Conversion.js

--- Basics/Conversion.js
+++ Basics/Conversion.js
@@ -20,8 +20,39 @@
 //""=> false
 //"Kartik"=> true
 
-let someNumber= 31
+// let someNumber= 31
 
-let stringNumber = String(someNumber);
-console.log(stringNumber);
-console.log(typeof stringNumber)
+// let stringNumber = String(someNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber)
+
+//****************OPERATIONS************ 
+
+
+// let value =3
+// let negValue= -value
+// console.log(negValue);
+
+//Basic Maths Operations
+
+// console.log(2+3);
+// console.log(2-2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(23%2);
+
+// let str1= "hello"
+// let str2= " Kartik"
+// let str3 = str1 + str2
+// console.log(str3);
+// console.log("1" + 2 + 4);
+// console.log(1 + 3 + "4");
+// console.log(1 + "3" + 5);
+
+
+
+// console.log(+true)
+// console.log(+"")
+
+
+
