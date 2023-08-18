311a5582fcbf051b440ab40db1845c0b92c930a8
KartikXx <kartik.rajput1714@gmail.com>
2023-08-19 00:13:35 +0530

    conversion of datatypes

---
+++ Basics/Conversion.js
@@ -0,0 +1,27 @@
+// let score = false
+
+// console.log(typeof score);
+// console.log(typeof (score));
+ 
+
+// let valueInnumber = Number(score)
+
+// console.log(typeof valueInnumber);
+// console.log(valueInnumber);
+
+//"33"= 33
+//"33abc"= NaN
+//true=1; false=0;
+// let isLoggedIn = ""
+// let booleanIsloggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsloggedIn);
+
+//1=>true; 0=> false
+//""=> false
+//"Kartik"=> true
+
+let someNumber= 31
+
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber)
